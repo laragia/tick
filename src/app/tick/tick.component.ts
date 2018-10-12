8f2116f25d504754d61c7bbe9739ae648379f7de
Lara Giannini <lara@Laras-MacBook-Pro.local>
2018-10-12 11:04:56 +0200

    begonnen Daten zu speichern

--- src/app/tick/tick.component.ts
+++ src/app/tick/tick.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material'
 import { TickDialogComponent } from './tick-dialog/tick-dialog.component'
+import { Tick } from './tick.model'
 
 @Component({
   selector: 'app-tick',
@@ -8,11 +9,14 @@ import { TickDialogComponent } from './tick-dialog/tick-dialog.component'
   styleUrls: ['./tick.component.css']
 })
 export class TickComponent implements OnInit {
-
+  private ticks: Tick[] = [];
   constructor(private matDialog: MatDialog) {
   }
 
   ngOnInit() {
+    // TODO read local storage
+    this.ticks = JSON.parse(localStorage.getItem('ticks'));
+
   }
 
   newTick() {
@@ -26,4 +30,16 @@ export class TickComponent implements OnInit {
       console.log('dialog was closed' + result);
     });
   }
+
+{
+  let t = { place: 'Kerns', date: new Date(), reminder: true, bodyLocation: 'head'};
+  let t2 = {};
+  t2.place = 'shgfhjsgfj';
+  this.ticks.push(t);
+  this.ticks.push(t2);
+  // TODO save in local storage
+  localStorage.setItem('ticks', JSON.stringify(this.ticks));
+}
+
+
 }
