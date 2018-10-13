560c40f06e61957d2d903a0a8d3fce34a6730577
Lara Giannini <lara@Laras-MacBook-Pro.local>
2018-10-13 19:41:26 +0200

    die eingetragenen Zecken werden aufgelistet

--- src/app/customMaterial.module.ts
+++ src/app/customMaterial.module.ts
@@ -1,12 +1,12 @@
-import { MatButtonModule, MatCheckboxModule, MatSlideToggleModule, MatFormFieldModule, MatNativeDateModule, MatDatepickerModule, MatInputModule, MatDialogModule} from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatSlideToggleModule, MatFormFieldModule, MatNativeDateModule, MatDatepickerModule, MatInputModule, MatDialogModule, MatListModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule, MatSlideToggleModule, MatFormFieldModule,
-    MatNativeDateModule, MatDatepickerModule, MatInputModule, BrowserAnimationsModule, MatDialogModule],
+    MatNativeDateModule, MatDatepickerModule, MatInputModule, BrowserAnimationsModule, MatDialogModule, MatListModule],
   exports: [MatButtonModule, MatCheckboxModule, MatSlideToggleModule, MatFormFieldModule,
-    MatNativeDateModule, MatDatepickerModule, MatInputModule, BrowserAnimationsModule, MatDialogModule],
+    MatNativeDateModule, MatDatepickerModule, MatInputModule, BrowserAnimationsModule, MatDialogModule, MatListModule],
 })
 export class CustomMaterialModule { }
